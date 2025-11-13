98fe2d7bddd99060887697c08001011fb10184c8
Sagarnaga-Jesus <23308060610335@cetis61.edu.mx>
2025-11-12 21:58:01 -0700

    dale luis

--- app.py
+++ app.py
@@ -1,17 +1,46 @@
 from flask import Flask,render_template, request, redirect, url_for, flash, session
 from datetime import datetime, timedelta
+import requests 
 
 app = Flask(__name__)
 app.secret_key = '1q2w3e4r5t6y7u8i9o0pp0o9i8u7y6t5r4e3w2q1'
+API='https://www.themealdb.com/api/json/v1/1/search.php?s='
 app.permanent_session_lifetime = timedelta(minutes=30)
 
 @app.route('/' , methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        buscar = request.form['buscar']
-        flash(f'Buscando: {buscar}', 'info')
         
     return render_template('base.html')
+
+@app.route('/buscar', methods=['POST'])
+def buscar():
+    comida = request.form.get('name', '').strip().lower()
+    
+    if not comida:
+        flash('Por favor ingresa un nombre de comida válido.', 'error')
+        return redirect(url_for('base'))
+    
+    try:
+        response = requests.get(f"{API}{comida }")
+        if response.status_code == 200:
+            comida_data = response.json()
+
+            comida_info = {
+                'name': comida_data['strMeal'].title(),
+                'categoria': comida_data['strCategory'],
+                'height': comida_data['strArea'],
+                'instrucciones': comida_data['strInstructions'],
+                'imagen': comida_data['strMealThumb'],
+            }
+            
+            return render_template('targeta.html', comidas=comida_info)
+        else:
+            flash(f'Comida "{comida}" no encontrada.', 'error')
+            return redirect(url_for('index'))
+        
+    except requests.exceptions.RequestException:
+        flash('Error al conectar con la API de comida. Inténtalo de nuevo más tarde.', 'error')
+        return redirect(url_for('index'))
 
 @app.route('/targeta', methods=['GET', 'POST'])
 def targeta():
